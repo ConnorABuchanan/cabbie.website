6b604ecfbbb7e0f8c3fca91c7c84fa61b5bd553f
ConnorABuchanan <buchanan.connor@icloud.com>
2025-05-02 10:57:04 -0700

    Started CSS and moved links to python list

--- cab_webapp/views.py
+++ cab_webapp/views.py
@@ -3,5 +3,11 @@ from . import models
 
 # Create your views here.
 
+project_info = [["https://gx.games/games/2gz94d/outofsight/tracks/91f267ee-2ca9-4943-9cf1-e8513b38fdf9/", "Out of Sight"],
+                ["https://github.com/ConnorABuchanan/DinoPrototype", "DinosTD Prototype"],
+                ["https://github.com/University-of-Utah-CS3505/a8-edu-app-JessP127", "Educational Cooking Game"],
+                ["https://github.com/University-of-Utah-CS3505/a7-sprite-editor-s23-gbrlhoopes", "Sprite Editor"]
+                ]
+
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"project_info": project_info})
