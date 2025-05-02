88e877ad1a4c79db6d185425edcfe5312fa5876f
ConnorABuchanan <buchanan.connor@icloud.com>
2025-05-02 14:37:27 -0700

    added thumbnails for each project and solidified a simple color scheme

--- cab_webapp/views.py
+++ cab_webapp/views.py
@@ -3,10 +3,10 @@ from . import models
 
 # Create your views here.
 
-project_info = [["https://gx.games/games/2gz94d/outofsight/tracks/91f267ee-2ca9-4943-9cf1-e8513b38fdf9/", "out of sight"],
-                ["https://github.com/ConnorABuchanan/DinoPrototype", "dinostd prototype"],
-                ["https://github.com/University-of-Utah-CS3505/a8-edu-app-JessP127", "educational cooking game"],
-                ["https://github.com/University-of-Utah-CS3505/a7-sprite-editor-s23-gbrlhoopes", "sprite editor"]
+project_info = [["https://gx.games/games/2gz94d/outofsight/tracks/91f267ee-2ca9-4943-9cf1-e8513b38fdf9/", "out of sight", "outofsightthumb.webp"],
+                ["https://github.com/ConnorABuchanan/DinoPrototype", "dinostd prototype", "dinosprototypethumb.png"],
+                ["https://github.com/University-of-Utah-CS3505/a8-edu-app-JessP127", "educational cooking game", "eduappthumb.png"],
+                ["https://github.com/University-of-Utah-CS3505/a7-sprite-editor-s23-gbrlhoopes", "sprite editor", "spriteeditorthumb.png"]
                 ]
 
 def index(request):
