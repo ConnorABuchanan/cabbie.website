f4ef397b941739f937f4a902cb63698b51397b9f
ConnorABuchanan <buchanan.connor@icloud.com>
2025-05-02 15:28:06 -0700

    started adding naughty or nice revamp. removed projects with private repos

--- cab_webapp/views.py
+++ cab_webapp/views.py
@@ -5,9 +5,11 @@ from . import models
 
 project_info = [["https://gx.games/games/2gz94d/outofsight/tracks/91f267ee-2ca9-4943-9cf1-e8513b38fdf9/", "out of sight", "outofsightthumb.webp"],
                 ["https://github.com/ConnorABuchanan/DinoPrototype", "dinostd prototype", "dinosprototypethumb.png"],
-                ["https://github.com/University-of-Utah-CS3505/a8-edu-app-JessP127", "educational cooking game", "eduappthumb.png"],
-                ["https://github.com/University-of-Utah-CS3505/a7-sprite-editor-s23-gbrlhoopes", "sprite editor", "spriteeditorthumb.png"]
+                ["/naughty-or-nice", "naughty or nice", "naughtyornicethumb.png"]
                 ]
 
 def index(request):
     return render(request, "index.html", {"project_info": project_info})
+
+def nice(request):
+    return render(request, "nice.html")
