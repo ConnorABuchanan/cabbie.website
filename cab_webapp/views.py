695196644d905875b8f16c8653b38379dc0aeb8b
ConnorABuchanan <buchanan.connor@icloud.com>
2025-05-02 14:14:18 -0700

    nav bar is fixed. added transparent images

--- cab_webapp/views.py
+++ cab_webapp/views.py
@@ -3,10 +3,10 @@ from . import models
 
 # Create your views here.
 
-project_info = [["https://gx.games/games/2gz94d/outofsight/tracks/91f267ee-2ca9-4943-9cf1-e8513b38fdf9/", "Out of Sight"],
-                ["https://github.com/ConnorABuchanan/DinoPrototype", "DinosTD Prototype"],
-                ["https://github.com/University-of-Utah-CS3505/a8-edu-app-JessP127", "Educational Cooking Game"],
-                ["https://github.com/University-of-Utah-CS3505/a7-sprite-editor-s23-gbrlhoopes", "Sprite Editor"]
+project_info = [["https://gx.games/games/2gz94d/outofsight/tracks/91f267ee-2ca9-4943-9cf1-e8513b38fdf9/", "out of sight"],
+                ["https://github.com/ConnorABuchanan/DinoPrototype", "dinostd prototype"],
+                ["https://github.com/University-of-Utah-CS3505/a8-edu-app-JessP127", "educational cooking game"],
+                ["https://github.com/University-of-Utah-CS3505/a7-sprite-editor-s23-gbrlhoopes", "sprite editor"]
                 ]
 
 def index(request):
