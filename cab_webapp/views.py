cdc7ca40d53bb9b2259951f8f38280e689ba57b1
ConnorABuchanan <buchanan.connor@icloud.com>
2025-05-05 13:14:22 -0700

    added images and some naughtynice changes

--- cab_webapp/views.py
+++ cab_webapp/views.py
@@ -5,7 +5,7 @@ from . import models
 
 project_info = [["https://gx.games/games/2gz94d/outofsight/tracks/91f267ee-2ca9-4943-9cf1-e8513b38fdf9/", "out of sight", "outofsightthumb.webp"],
                 ["https://github.com/ConnorABuchanan/DinoPrototype", "dinostd prototype", "dinosprototypethumb.png"],
-                ["/naughty-or-nice", "naughty or nice", "naughtyornicethumb.png"]
+                ["/naughty-or-nice", "naughty or nice", "present.png"]
                 ]
 
 def index(request):
